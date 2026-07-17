943d79f9e264cdc3e74d24055b7d9ee3ac6e91de
Henry Solarte <henrysolarte@hotmail.com>
2026-07-17 15:38:53 -0500

    fix: configurar multiples entradas html

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        historia: resolve(__dirname, 'historia.html'),
+        juntadirectiva: resolve(__dirname, 'juntadirectiva.html'),
+      },
+    },
+  },
 })
