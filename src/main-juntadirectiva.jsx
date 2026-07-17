7bacc597c7213a2152d0d6ce6f00edc38d39ccee
Henry Solarte <henrysolarte@hotmail.com>
2026-07-17 15:42:38 -0500

    fix: corregir import para render

--- src/main-juntadirectiva.jsx
+++ src/main-juntadirectiva.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import JuntaDirectiva from './components/juntadirectiva.jsx';
+import JuntaDirectiva from './components/JuntaDirectiva.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
